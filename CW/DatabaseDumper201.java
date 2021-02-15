b4253af9198daf99e256c96d66e3d33abbc61c0d
Daniel Lennon <d.lennonbeato@lancaster.ac.uk>
2021-02-15 13:31:14 +0000

    Added in type parsing

--- CW/DatabaseDumper201.java
+++ CW/DatabaseDumper201.java
@@ -56,45 +56,38 @@ public class DatabaseDumper201 extends DatabaseDumper
         return result;
     }
 
-    /*@Override
+    @Override
     public List<String> getViewNames() 
     {
         List<String> result = new ArrayList<>();
 
         try 
         {
-            String[] VIEW_TYPES = {"VIEW"};
             DatabaseMetaData md = this.getConnection().getMetaData();
-            ResultSet rs = md.getTables(null, null, null, new String[]{"VIEW"});
+            ResultSet rs = md.getTables(null, null, "%", new String[]{"VIEW"});
 
-            Statement stmt = getConnection().createStatement();
-            //ResultSet rs1 = stmt.executeQuery("SELECT TABLE_SCHEMA,TABLE_NAME FROM INFORMATION_SCHEMA.VIEWS");
-            //ResultSet rs1 = stmt.executeQuery("SELECT * FROM sqlite_master WHERE type = 'view'");
-            ResultSet rs1 = stmt.executeQuery("SELECT name FROM sqlite_master WHERE type = 'view' AND sql LIKE '% FROM %tablename% WHERE %';");
-            
-
-            System.out.println("hello there");
-            while (rs1.next()) 
+            while (rs.next()) 
             {
                 System.out.println("hi");
-                result.add(rs1.getString(1));
+                result.add(rs.getString(1));
             }
-            System.out.println(result);
         } 
         catch (Exception e) 
         {
             //TODO: handle exception
             System.out.println("exception");
         }
-
+        
+        System.out.println("Views are:");
+        System.out.println(result);
 
         return result;
-    }*/
+    }
 
     /**
      * Method used to get the view names of the database.
      */
-    public List<String> getViewNames() 
+    /*public List<String> getViewNames() 
     {
         List<String> result = new ArrayList<>();
         String query = "SELECT name FROM sqlite_master WHERE type = 'view' AND sql LIKE '% FROM %tablename% WHERE %';";
@@ -123,7 +116,7 @@ public class DatabaseDumper201 extends DatabaseDumper
         }
 
         return result;
-    }
+    }*/
 
     /**
      * get the DDL which creates a table given a string as input which represents the table name.
@@ -136,20 +129,17 @@ public class DatabaseDumper201 extends DatabaseDumper
         {
             List<String> namesList = this.getTableNames();
             DatabaseMetaData md = this.getConnection().getMetaData();
-            returnString = "CREATE TABLE (";
+            returnString = "CREATE TABLE ";
             for (String name : namesList) 
             {
                 
                 if(name.equals(input))
                 {
-                    returnString += input + ") ";
+                    returnString += input + " (";
                     System.out.println(name + "=" + input);
-                    //get that tables attributes and create the table
-                    //ResultSet rs = md.getTables(input, null, null, null);
-                    //System.out.println(rs);
 
                     Statement stmt = super.getConnection().createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT * FROM heroes");
+                    ResultSet rs = stmt.executeQuery("SELECT * FROM " + input);
 
                     System.out.println(rs);
                     
@@ -159,11 +149,11 @@ public class DatabaseDumper201 extends DatabaseDumper
                     for (int i = 1; i <= columnsNumber; i++) 
                     {
                         
-                        returnString += rsmd.getColumnName(i);
+                        returnString += rsmd.getColumnName(i) + " " + JDBCType.valueOf(rsmd.getColumnType(i));
                         
                         if (i == columnsNumber)
                         {
-                            //do nothing
+                            returnString += ")";
                         }
                         else
                         {
@@ -249,7 +239,7 @@ public class DatabaseDumper201 extends DatabaseDumper
                         {
                             
                             String columnValue = rs2.getString(i);
-                            values += columnValue;
+                            values += "'" + columnValue + "'";
                             
                             if (i == columnsNumber2)
                             {
@@ -310,6 +300,8 @@ public class DatabaseDumper201 extends DatabaseDumper
         this.getInsertsForTable("planets");
         this.getInsertsForTable("powers");
         this.getInsertsForTable("missions");
+        this.getDDLForTable("heroes");
+        this.getDDLForTable("planets");
         this.getViewNames();
     }
 
